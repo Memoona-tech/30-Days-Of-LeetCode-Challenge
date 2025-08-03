b50f08337ee9734769e71eb61f924dd643a2d110
Memoona Saleem <156640976+Memoona-tech@users.noreply.github.com>
2025-08-04 04:27:01 +0500

    Create 345: Reverse Vowels of String (Easy).py

---
+++ 345: Reverse Vowels of String (Easy).py
@@ -0,0 +1,20 @@
+
+# solution
+# --------------------   O(n) TC and O(n) SC ---------------- 
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = set("aeiouAEIOU")
+        s = list(s)                     # O(n) SC 
+        left, right = 0, len(s)-1
+
+        while left < right:
+            if s[left] not in vowels:
+                left += 1
+            elif s[right] not in vowels:
+                right -= 1
+            else:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+        return ''.join(s)     # O(n) SC 
+        

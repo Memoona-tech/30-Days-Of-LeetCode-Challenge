0edd4f2d57cc1c84c061a1d283fc820d1f5a5eaa
Memoona Saleem <156640976+Memoona-tech@users.noreply.github.com>
2025-08-06 23:56:12 +0500

    Update 136 – Single Number (Easy).py

--- 136 – Single Number (Easy).py
+++ 136 – Single Number (Easy).py
@@ -15,3 +15,25 @@ class Solution:
     Group them:
     (2 ^ 2) ^ (3 ^ 3) ^ 5 = 0 ^ 0 ^ 5 = 5
 }
+
+
+# Solution 2
+# ------------- O(n) TC ----------- O(n) SC ----------
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # 1) Count how many times each number appears
+        frequency = {}
+        for num in nums:
+            if num in frequency:
+                frequency[num] += 1
+            else:
+                frequency[num] = 1
+
+        # 2) Find and return the one whose count is exactly 1
+        for num, count in frequency.items():
+            if count == 1:
+                return num
+
+
+

824df597ddc8c7591f088fe0bb5b6f17a546e271
Memoona Saleem <156640976+Memoona-tech@users.noreply.github.com>
2025-08-08 23:01:34 +0500

    Create 36: Valid Sudoku (Medium).py

---
+++ 36: Valid Sudoku (Medium).py
@@ -0,0 +1,22 @@
+# SOLUTION 1
+# ------------------ O(1) TC for 9 x 9 fixed no. of cols&rows----------- O(1) SC --------
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[( r // 3, c // 3)]:
+                    return False
+
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                boxes[(r//3, c//3)].add(board[r][c])                
+
+        return True

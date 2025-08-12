2fb8bd143e8012cd59c6c36526e6762116d6f258
Memoona Saleem <156640976+Memoona-tech@users.noreply.github.com>
2025-08-12 21:14:38 +0500

    Create 102 – Binary Tree Level Order Traversal (Medium).py

---
+++ 102 – Binary Tree Level Order Traversal (Medium).py
@@ -0,0 +1,34 @@
+# SOLUTION 1
+# ------------------ O(n) TC ----------- O(w) SC {The queue stores at most one level of the tree at any time. In the worst case (a balanced tree), this is (O(w)), where (w) is the maximum width of the tree.}--------
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            level_nodes = []
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level_nodes.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            res.append(level_nodes)
+            
+        return res
+                
+    

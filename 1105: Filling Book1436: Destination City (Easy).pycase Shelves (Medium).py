5330a318a780880c5fc15be1663d767082df76cd
Memoona Saleem <156640976+Memoona-tech@users.noreply.github.com>
2025-08-16 01:18:55 +0500

    Create 1105: Filling Book1436: Destination City (Easy).pycase Shelves (Medium).py

---
+++ 1105: Filling Book1436: Destination City (Easy).pycase Shelves (Medium).py
@@ -0,0 +1,26 @@
+ # SOLUTION 1
+# ------------------ O(n^2) TC ----------- O(1) SC --------
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        for i in range(len(paths)):
+            dest = paths[i][1]
+            foundAtStart = False
+
+            for j in range(len(paths)):
+                if dest == paths[j][0]:
+                    foundAtStart = True
+                    break
+            if not foundAtStart:
+                return dest
+
+# SOLUTION 2
+# ------------------ O(n^2) TC ----------- O(1) SC --------
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        start_cities = {path[0] for path in paths}  # set of all start cities
+
+        for _, end in paths:  # loop through all destination cities
+            if end not in start_cities:
+                return end

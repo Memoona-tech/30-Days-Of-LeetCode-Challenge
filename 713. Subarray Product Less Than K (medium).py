a71ea245f40bc2b99098cc19258da620a4bf95d8
Memoona Saleem <156640976+Memoona-tech@users.noreply.github.com>
2025-12-23 22:32:21 +0500

    Update 713. Subarray Product Less Than K (medium).py

--- 713. Subarray Product Less Than K (medium).py
+++ 713. Subarray Product Less Than K (medium).py
@@ -17,3 +17,45 @@ class Solution:
             c += 1 + (r - l)
             r += 1
         return c
+
+
+# SOLUTION 2
+# ------------------ O(n^2) TC ----------- O(1) SC --------
+
+def numSubarrayProductLessThanK(nums, k):
+    count = 0
+    n = len(nums)
+    
+    for i in range(n):
+        prod = 1
+        for j in range(i, n):
+            prod *= nums[j]
+            if prod < k:
+                count += 1
+            else:
+                break
+    return count
+
+# SOLUTION 3
+# ------------------ O(n log n) TC ----------- O(n) SC --------
+
+import math
+import bisect
+
+def numSubarrayProductLessThanK(nums, k):
+    if k <= 1:
+        return 0
+    
+    logs = [0]
+    for x in nums:
+        logs.append(logs[-1] + math.log(x))
+    
+    logk = math.log(k)
+    count = 0
+    
+    for i in range(len(nums)):
+        j = bisect.bisect_left(logs, logs[i] + logk, i + 1)
+        count += j - i - 1
+    
+    return count
+

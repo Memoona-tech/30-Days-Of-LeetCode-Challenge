3437046458ddfd2ea7e295b902f27406b2631942
Memoona-tech <156640976+Memoona-tech@users.noreply.github.com>
2026-01-08 13:00:06 +0500

    Create 525. Contiguous Array (medium).py

---
+++ 525. Contiguous Array (medium).py
@@ -0,0 +1,23 @@
+# SOLUTION 1
+# ------------------ O(n) TC ----------- O(n) SC --------
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        zero, one = 0, 0
+        res = 0
+        diff_idx = {}
+        for i, n in enumerate(nums):
+            if n == 0:
+                zero += 1
+            if n == 1:
+                one += 1
+            
+            if one - zero not in diff_idx:
+                diff_idx[one - zero] = i
+            
+            if one == zero:
+                res = one + zero
+            else:
+                idx = diff_idx[one - zero]
+                res = max(res, i - idx)        
+        return res

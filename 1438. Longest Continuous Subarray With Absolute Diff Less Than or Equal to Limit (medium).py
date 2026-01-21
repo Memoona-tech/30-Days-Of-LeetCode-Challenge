f1f62a84c7943c08f9b202dfa4f5edb030ef8089
Memoona-tech <156640976+Memoona-tech@users.noreply.github.com>
2026-01-21 12:10:13 +0500

    Implement solution for longest subarray problem

---
+++ 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit (medium).py
@@ -0,0 +1,27 @@
+# SOLUTION 1
+# ------------------ O(n) TC ----------- O(n) SC --------
+
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        min_q = deque() # mono increasing
+        max_q = deque() # mono increasing
+        res = 0
+        l = 0
+
+        for r in range(len(nums)):
+            while min_q and nums[r] < min_q[-1]:
+                min_q.pop()
+            while max_q and nums[r] > max_q[-1]:
+                max_q.pop()
+            min_q.append(nums[r])
+            max_q.append(nums[r])
+
+            while max_q[0] - min_q[0] > limit:
+                if nums[l] == max_q[0]:
+                    max_q.popleft()
+                if nums[l] == min_q[0]:
+                    min_q.popleft()
+                l += 1
+            res = max(res, r - l + 1)
+
+        return res 

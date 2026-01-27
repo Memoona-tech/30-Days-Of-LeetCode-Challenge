df48701b80d50f8de7be76a26e302f67a88b3e08
Memoona-tech <156640976+Memoona-tech@users.noreply.github.com>
2026-01-27 14:43:51 +0500

    Add brute force solution for sliding window maximum
    Implemented a brute force solution for the sliding window maximum problem, calculating the maximum for each window of size k.

--- 239. Sliding Window Maximum (hard).py
+++ 239. Sliding Window Maximum (hard).py
@@ -23,3 +23,20 @@ class Solution:
             r += 1
         return res
 
+
+# SOLUTION 2 --> BRUTE FORCE
+# Each max(nums[l:r+1]) takes O(k) 
+# ------------------ O(N * K) TC ----------- O(1) SC --------
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) == 1:
+            return nums
+        l = 0
+        r = k - 1
+        res = []
+        while r < len(nums):
+            res.append(max(nums[l:r+1]))
+            l += 1
+            r += 1
+        return res

04cce112ec0d536adf3460832e4f683d8c03277e
Memoona-tech <156640976+Memoona-tech@users.noreply.github.com>
2026-03-12 00:06:26 +0500

    Create 1009: Complement (easy).py

---
+++ 1009: Complement (easy).py
@@ -0,0 +1,14 @@
+# SOLUTION 1
+# ------------------ O(n) TC ----------- O(1) SC --------
+
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        if n == 0: return 1
+        if n == 1: return 0
+        def mask(n):
+            m = 1
+            while m <= n:
+                m *= 2
+            return m
+        return n^(mask(n)-1)
+        
